013209ed8663b51f23cf08d9acaddcc761e65f34
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-07-14 09:02:17 +0530

    Update Github.js

--- src/Components/About/Github.js
+++ src/Components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="kishan-k9"
+        username="kishankumar-dev"
         blockSize={15}
         blockMargin={5}
         // color="#c084f5"
